aef5b32e86da2048c0755962c61c213728ed32a6
cwrobertson16 <cwrobertson@berklee.edu>
2016-10-29 22:51:46 -0400

    Initial Commit
    Initial Commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	public Text countText;
+
+
+
+	private Rigidbody rb;
+	private int count;
+	private float timeLeft = 3f;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+		count = 0;
+		SetCountText ();
+	}
+
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * speed);
+
+
+		//makes ball jump into air when space bar is pressed
+
+		if (Input.GetButtonDown("Jump")) {
+			GetComponent<Rigidbody>().AddForce(new Vector3(0, 3, 0),ForceMode.Impulse);
+		}
+
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.CompareTag ("Pick Up"))
+		{
+			other.gameObject.SetActive (false);
+			count = count + 1;
+			SetCountText ();
+		}
+	}
+
+	void SetCountText ()
+	{
+		countText.text = "Count: " + count.ToString ();
+
+	}
+}
